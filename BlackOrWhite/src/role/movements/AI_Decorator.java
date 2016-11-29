bcbb7d1e3a29b06069ecea19ea8620cb9bb47388
WaterBall <johnny850807@gmail.com>
2016-11-29 22:23:27 +0800

    add map item pics

--- BlackOrWhite/src/role/movements/AI_Decorator.java
+++ BlackOrWhite/src/role/movements/AI_Decorator.java
@@ -1,9 +1,12 @@
 package role.movements;
 
+import java.util.Random;
+
 import role.AI;
 
 public abstract class AI_Decorator implements AI_Movement {
 	protected AI_Movement movement; // decorated object
+	protected Random random = new Random(); //the random object let subclass use conveniently
 	
 	public AI_Decorator(AI_Movement wrapped){
 		this.movement = wrapped;
--- BlackOrWhite/src/role/movements/AI_Halt.java
+++ BlackOrWhite/src/role/movements/AI_Halt.java
@@ -11,8 +11,14 @@ public class AI_Halt extends AI_Decorator {
 	@Override
 	public void randomChoose(AI ai) {
 		// 40% to halt about a while ...
-		
-		
+		if ( random.nextInt(100) >= 60 )
+			halt();
+		else
+			movement.randomChoose(ai);
+	}
+	
+	public void halt(){
+		// halt algorithm
 	}
 
 }
